d0410fb9b0eaa0b978df7c1dcdf0e726d0caf339
Aleksander Matusiak <am347171@students.mimuw.edu.pl>
2016-03-13 12:52:13 +0100

    Add default functions for Ix class + add Char and Integer instances

--- MyArray.hs
+++ MyArray.hs
@@ -2,14 +2,28 @@ module MyArray where
 
 class Ord a => Ix a where
   range :: (a, a) -> [a]
+  
   index :: (a, a) -> a -> Int
+  index (beg, end) i =
+    head [ n | (x, n) <- zip (range (beg, end)) ([0..(rangeSize(beg, end))]), x == i  ]
+  
   inRange :: (a, a) -> a -> Bool
+  inRange (beg, end) i = elem i $ range (beg, end)
 
   rangeSize :: (a, a) -> Int
   rangeSize = length . range
 
+instance Ix Char where
+  range (beg, end) = [beg..end]
+
 instance Ix Int where
   range (beg, end) = [beg..end]
   index (beg, _) i = i - beg
-  inRange (beg, end) i = (i >= beg) || (i <= end)
+  inRange (beg, end) i = (i >= beg) && (i <= end)
+
+instance Ix Integer where
+  range (beg, end) = [beg..end]
+
+-- instance (Ix a, Ix b) => Ix (a, b) where
+  
   
