c7c41e2a094d0ba57f5a306aefbac1b6b96741b9
Aleksander Matusiak <am347171@students.mimuw.edu.pl>
2016-03-23 15:00:13 +0100

    Add using assumption that graph is in good format

--- MyGraph.hs
+++ MyGraph.hs
@@ -2,23 +2,17 @@ module MyGraph(dfs, graph) where
 import MyArray
 import Data.Set as Set
 
-type Graph = ((Int, Int), Array Int [Int])
+type Graph = Array Int [Int]
 
 graph :: (Int, Int) -> [(Int, [Int])] -> Graph
-graph ran@(beg, end) l =
-  (ran, array ran newList)
-  where
-    newList = l ++ [(x, []) | x <- [beg..end]]
+graph = array
 
 dfs :: Graph -> Int -> [Int]
 dfs g v = Set.elems $ dfsAux g v empty
 
 dfsAux :: Graph -> Int -> Set.Set Int -> Set.Set Int
-dfsAux g@(ran, arr) v vis =
-  if inRange ran v then
-    Prelude.foldl visit (insert v vis) $ arr ! v
-  else
-    insert v vis
+dfsAux g v vis =
+  Prelude.foldl visit (insert v vis) $ g ! v
   where
     visit s u =
       if member u s then
